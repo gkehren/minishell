3bb4cd26e9361ff33284a0da446963785f7755c7
genouf <gabriel.enouf@yahoo.fr>
2022-09-29 10:07:13 +0200

    [SAVE 42]

--- sources/expanser/expand_word.c
+++ sources/expanser/expand_word.c
@@ -2,6 +2,9 @@
 
 // static char *join_env_word(char *begin, char *expand)
 // {
+// 	char	*result;
+
+// 	result = ft_strjoin(begin, expand);
 
 // }
 
@@ -35,19 +38,19 @@ static void	find_size_title(char *str, int *i)
 	}
 }
 
-char	*find_title_expand(char *str)
+static char	*find_title_expand(char **str)
 {
 	int		i;
 	int		j;
 	char	*title;
 
-	find_size_title(str, &i);
+	find_size_title(*str, &i);
 	title = (char *)malloc(sizeof(char) * (i + 1));
 	if (title == NULL)
 		return (NULL);
 	if (i == 1)
 	{
-		title[0] = str[0];
+		title[0] = (*str)[0];
 		title[1] = '\0';
 	}
 	else
@@ -55,11 +58,12 @@ char	*find_title_expand(char *str)
 		j = 0;
 		while (j < i)
 		{
-			title[j] = str[j];
+			title[j] = (*str)[j];
 			j++;
 		}
 		title[j] = '\0';
 	}
+	*str += i;
 	return (title);
 }
 
@@ -69,7 +73,6 @@ int	expand_word(t_expanse expanse, t_list *venv, char *str, int i)
 	char	*begin;
 	char	*tmp;
 
-	(void)venv;
 	if (expanse.mode == PASS)
 		return (0);
 	begin = (char *)malloc(sizeof(char) * (i + 1));
@@ -82,9 +85,12 @@ int	expand_word(t_expanse expanse, t_list *venv, char *str, int i)
 		j++;
 	}
 	begin[j] = '\0';
-	printf("last word is %c\n", begin[j - 1]);
-	tmp = find_title_expand(str + i + 1);
+	//printf("last word is %c\n", begin[j - 1]);
+	str += (i + 1);
+	tmp = find_title_expand(&str);
+	printf("debut str = %c\n", *str);
 	tmp = find_env_word(venv, tmp);
-	// join_env_word;
+	printf("content of expand is %s\n", tmp);
+	//join_env_word(begin, tmp);
 	return (0);
 }
