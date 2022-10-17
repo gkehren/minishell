d6be5d9005e32964a3606558a0bf89592f8f99aa
gkehren <kehren.guillaume@gmail.com>
2022-10-17 21:47:32 +0200

    [ADD] heredoc & fix file redirection

--- sources/exec/child.c
+++ sources/exec/child.c
@@ -1,56 +1,57 @@
 #include "minishell.h"
 
-void	child_process(int *fd, int *fdd, char **env, t_list *lcmd)
+void	redirect_child(t_list *lcmd, t_cmd *cmd, int *fd, int *fdd)
+{
+	if (cmd->files->infile != -2)
+	{
+		dup2(cmd->files->infile, STDIN_FILENO);
+		close(cmd->files->infile);
+	}
+	else
+		dup2((*fdd), STDIN_FILENO);
+	if (cmd->files->outfile != -2)
+	{
+		dup2(cmd->files->outfile, STDOUT_FILENO);
+		close(cmd->files->outfile);
+	}
+	else
+	{
+		if (lcmd->next != NULL)
+			dup2(fd[1], STDOUT_FILENO);
+	}
+	close(fd[0]);
+	close(fd[1]);
+}
+
+void	child_process(int *fd, int *fdd, t_list *venv, t_list *lcmd)
 {
 	t_cmd	*cmd;
 	char	*path;
+	char	**env;
 
 	cmd = (t_cmd *)lcmd->content;
+	env = send_env(&venv);
 	if (!cmd->full_cmd)
 		exit(0);
 	path = path_command(cmd->full_cmd[0], env);
 	if (!path)
 		return (perror("Minishell"), free_double_tab((void *)env), free(path));
+	redirect_child(lcmd, cmd, fd, fdd);
+	if (execve(path, cmd->full_cmd, env) == -1)
+		return (free(path), perror("Minishell"), free_double_tab((void *)env));
+}
+
+void	child_process_builtins(int *fd, int *fdd, t_list *venv, t_list *lcmd, t_builtins builtins)
+{
+	t_cmd	*cmd;
+
+	cmd = (t_cmd *)lcmd->content;
 	close(fd[0]);
 	dup2((*fdd), STDIN_FILENO);
 	if (lcmd->next != NULL)
 		dup2(fd[1], STDOUT_FILENO);
-	if (execve(path, cmd->full_cmd, env) == -1)
-		return (perror("Minishell"), free_double_tab((void *)env), free(path));
+	(*builtins)(cmd->argc, cmd->full_cmd, &venv);
+	ft_lstclear(&lcmd, &del_cmd);
+	ft_lstclear(&venv, &del_venv);
 	exit(0);
-}
-
-void	child_process_out(int *fd, int *fdd, char **env, t_list *lcmd)
-{
-	t_cmd	*cmd;
-	char	*path;
-
-	(void)fdd;
-	cmd = (t_cmd *)lcmd->content;
-	path = path_command(cmd->full_cmd[0], env);
-	if (!path)
-		return (perror("Minishell"), free_double_tab((void *)env), free(path));
-	dup2(fd[0], STDIN_FILENO);
-	close(fd[1]);
-	dup2(cmd->files->outfile, STDOUT_FILENO);
-	if (execve(path_command(cmd->full_cmd[0], env), cmd->full_cmd, env) == -1)
-		return (free_double_tab((void *)env), free(path));
-	close(cmd->files->outfile);
-}
-
-void	child_process_in(int *fd, int *fdd, char **env, t_list *lcmd)
-{
-	t_cmd	*cmd;
-	char	*path;
-
-	(void)fdd;
-	cmd = (t_cmd *)lcmd->content;
-	path = path_command(cmd->full_cmd[0], env);
-	if (!path)
-		return (perror("Minishell"), free_double_tab((void *)env), free(path));
-	close(fd[0]);
-	dup2(cmd->files->infile, STDIN_FILENO);
-	if (execve(path_command(cmd->full_cmd[0], env), cmd->full_cmd, env) == -1)
-		return (free_double_tab((void *)env), free(path));
-	close(cmd->files->infile);
 }
