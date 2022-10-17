d71bac62d02a153a9203793893af07acaa23a49d
genouf <gabriel.enouf@yahoo.fr>
2022-10-17 21:49:15 +0200

    [ADD] only_builtin v2

--- sources/exec/builtins_exec.c
+++ sources/exec/builtins_exec.c
@@ -4,21 +4,23 @@ int	only_builtins(t_builtins p_builtins, t_cmd *cmd, t_list *venv)
 {
 	int	tmp_stdin;
 	int	tmp_stdout;
-	//int	check1;
-	//int	check2;
 
 	tmp_stdin = dup(STDIN_FILENO);
 	tmp_stdout = dup(STDOUT_FILENO);
-	if (tmp_stdin == -1 || tmp_stdout == -1)
-		return (print_error("Dup failed !\n"));
-	//check1 = dup2(cmd->infile.fd, STDIN_FILENO);
-	//check2 = dup2(cmd->outfile.fd, STDOUT_FILENO);
-	//if (check1 == -1 || check2 == -1)
-	//	return (print_error("Dup2 failed !\n"));
+	if (cmd->files->infile != -2)
+		dup2(cmd->files->infile, STDIN_FILENO);
+	if (cmd->files->outfile != -2)
+		dup2(cmd->files->outfile, STDOUT_FILENO);
 	(*p_builtins)(cmd->argc, cmd->full_cmd, &venv);
-	dup2(tmp_stdin, STDIN_FILENO);
-	dup2(tmp_stdout, STDOUT_FILENO);
-	//if (check1 == -1 || check2 == -1)
-	//	return (print_error("Dup2 failed !\n"));
-	return (0);
+	if (cmd->files->infile != -2)
+	{
+		dup2(tmp_stdin, STDIN_FILENO);
+		close(cmd->files->infile);
+	}
+	if (cmd->files->outfile != -2)
+	{
+		dup2(tmp_stdout, STDOUT_FILENO);
+		close(cmd->files->outfile);
+	}
+	return (close(tmp_stdin), close(tmp_stdout), 0);
 }
