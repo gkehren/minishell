ac2e7e115927fced6ff2d3af09001b173ca4c581
genouf <gabriel.enouf@yahoo.fr>
2022-10-26 00:42:23 +0200

    [NORM] first end parsing

--- includes/lexer.h
+++ includes/lexer.h
@@ -8,6 +8,7 @@ t_list		*generate_token(char *entry, int mode);
 
 /*						TOKEN_CREATION						*/
 int			create_spe_token(t_list **token_list, int analyse);
-int			create_mand_token(t_list **token_list, char *entry, int *i, int mode);
+int			create_mand_token(t_list **token_list, char *entry,
+				int *i, int mode);
 
 #endif
--- sources/parser/parser.c
+++ sources/parser/parser.c
@@ -14,10 +14,9 @@ static t_cmd	*init_current_cmd(void)
 	return (current_cmd);
 }
 
-static int	process_cmd(t_list **tmp_list, t_cmd *current_cmd)
+static int	process_cmd(t_list **tmp_list, t_cmd *current_cmd,
+	t_token_lex *tmp_content)
 {
-	t_token_lex	*tmp_content;
-
 	while (*tmp_list)
 	{
 		tmp_content = (t_token_lex *)(*tmp_list)->content;
@@ -27,20 +26,16 @@ static int	process_cmd(t_list **tmp_list, t_cmd *current_cmd)
 					tmp_content->content);
 			if (current_cmd->full_cmd == NULL)
 				return (del_cmd(current_cmd), 1);
-			(*tmp_list) = (*tmp_list)->next;
 		}
 		else if (tmp_content->token == PIPE)
-		{
-			(*tmp_list) = (*tmp_list)->next;
-			return (0);
-		}
+			return ((*tmp_list) = (*tmp_list)->next, 0);
 		else
 		{
 			if (new_token_files(&current_cmd->token_files, tmp_content,
 					(t_token_lex *)(*tmp_list)->next->content))
 				return (1);
-			(*tmp_list) = (*tmp_list)->next->next;
 		}
+		(*tmp_list) = (*tmp_list)->next;
 	}
 	return (0);
 }
@@ -83,7 +78,7 @@ t_list	*generate_cmd(t_list **token_list, t_list **venv)
 		return (ft_lstclear(token_list, &del_token_lex), NULL);
 	while (tmp_list)
 	{
-		if (process_cmd(&tmp_list, current_cmd))
+		if (process_cmd(&tmp_list, current_cmd, NULL))
 			return (free_parser(token_list, venv, cmd_list), NULL);
 		if (add_cmd(&cmd_list, &current_cmd, tmp_list))
 			return (free_parser(token_list, venv, cmd_list), NULL);
